fe172136614f2620b9406b06d1f870d58519488b
MikaAK <me@mikakalathil.ca>
2016-05-16 12:28:01 -0700

    Get it working for fuse

--- index.d.ts
+++ index.d.ts
@@ -2,35 +2,36 @@
 // Project: https://github.com/krisk/Fuse
 // Definitions by: Mika Kalathil <https://github.com/mikaak/>
 
-export interface FuseOptions {
-  id?: string;
-  keys?: any[];
-  caseSensitive?: boolean;
-  include?: string[];
-  maxPatternLength?: number;
-  shouldSort?: boolean;
-  distance?: number;
-  location?: number;
-  threshold?: number;
-  verbose?: boolean;
-  tokenize?: boolean;
-  searchFn?(searchFunction: FuseSearchFn);
-  getFn?(object: any, property: string): any;
-  sortFn?(): any[];
+declare module 'fuse.js' {
+  interface FuseOptions {
+    id?: string;
+    keys?: any[];
+    caseSensitive?: boolean;
+    include?: string[];
+    maxPatternLength?: number;
+    shouldSort?: boolean;
+    distance?: number;
+    location?: number;
+    threshold?: number;
+    verbose?: boolean;
+    tokenize?: boolean;
+    searchFn?(searchFunction: FuseSearchFn);
+    getFn?(object: any, property: string): any;
+    sortFn?(): any[];
+  }
+
+  interface FuseSearchFn {
+    constructor(pattern: string, options: FuseOptions);
+    search(pattern: string): any[];
+  }
+
+  export = class Fuse<T> {
+    public list: T[];
+    public options: FuseOptions;
+
+    constructor(list: T[], options: FuseOptions);
+    public set(list: T[]): T[];
+    public search(pattern: string): T[];
+  };
 }
-
-export interface FuseSearchFn {
-  constructor(pattern: string, options: FuseOptions);
-  search(pattern: string): any[];
-}
-
-export class Fuse<T> {
-  public list: T[];
-  public options : FuseOptions;
-
-  constructor(list: T[], options: FuseOptions);
-  public set(list: T[]): T[];
-  public search(pattern: string): T[];
-}
-
 
